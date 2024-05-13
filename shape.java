417525d48ceb7883c32fcb5868b4f401c716f684
PrajaktaBhagwat2001 <100439224+PrajaktaBhagwat2001@users.noreply.github.com>
2024-05-13 17:40:08 +0530

    Add files via upload

---
+++ shape.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public abstract class shape {
+   
+	public abstract void Shape_demo();
+	
+	public void displsy() { 
+		System.out.println("We are calculating Area of:\n1.Circle\n2.Rectangle\n3Square");
+	}
+}
---
+++ square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class square extends shape{
+	public void Shape_demo() { 
+		Scanner sc=new Scanner(System.in);
+		 System.out.println("Enter length of Square:");
+		 int r=sc.nextInt();
+		 float area=(float) (r*r);
+		 System.out.println("Area of Square is: "+area);
+		
+	}
+
+
+}
